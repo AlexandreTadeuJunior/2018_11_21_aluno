b0a169d135085523e7ba8953467fca6981209261
AlexandreTadeuJunior <alexandre.tadeu1995@gmail.com>
2018-12-23 13:26:07 -0200

    Created a controller admission

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :students
+      resources :students, :admission
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     end
   end
