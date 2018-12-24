14822674f3d4a11ba45c77ff6815e27d4971eeb9
AlexandreTadeuJunior <alexandre.tadeu1995@gmail.com>
2018-12-24 12:03:19 -0200

    Modified test

--- test/test.rb
+++ test/test.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 # created a student
 response = HTTParty.post("http://localhost:3000/api/v1/students",
-                          :body => {"student[name]": "Joao S", "student[cpf]": "148.917.040-64" , "enem_grade": "530"})
+                          :body => {"student[name]": "Alexandre Tadeu da Silva Junior", "student[cpf]": "366.179.540-68" , "enem_grade": "530"})
 p "Created student"
 p "Response code: #{response.code}"
 p "Response body: #{response.body}"
